066f7c1368fc2044e4fec54d1e82291a02b6b76f
aritse23 <aritse23@gmail.com>
2015-10-14 06:00:07 +1300

    donnnneee 5.59am 14/10/15

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -40,13 +40,13 @@ document.addEventListener("DOMContentLoaded",function(){
 	    //Else if the img Loc is equal to and the same type as "assets/img/icons/kick_1.png"
 	    //also if the amount of sections after .current is greater than 0 then run following code
 	  	} else if (imgLoc === 'assets/img/icons/kick_1.png' && $('.current').prev('section.section').length > 0) {
-	        var $prev = $('.current').prev('.section');
-	        var top = $prev.offset().top;	        
-	        $('.current').removeClass('current');	      
-	        $('body').animate({
-	          scrollTop: top
+	        var $prev = $('.current').prev('.section'); //selects section with class current, then gets the section prior to current
+	        var top = $prev.offset().top;	        	//sets variable top to the co-ord of the top of previous section
+	        $('.current').removeClass('current');	     //removes class current and all child
+	        $('body').animate({							//creates animation on body element
+	          scrollTop: top				//scrolls to the top of previous section
 	        }, function () {
-	               $prev.addClass('current');
+	               $prev.addClass('current');	//gives that prev section a class of current
 	        });
 	  	}
 
@@ -54,13 +54,13 @@ document.addEventListener("DOMContentLoaded",function(){
 	});
 	
 	//<===============NAV MENU=========================>
-	$(".nav_links").on("click",function(){
-		var new_current = $(this).attr("href");	//creates variable named imgLoc which stores the clicked link's href
-	    //alert($(".current").attr("id"));
-	    $(".current").removeClass("current");
+	$(".nav_links").on("click",function(){		//if ckicking class
+		var new_current = $(this).attr("href");	//creates variable which stores the clicked link's href
+	    //alert($(".current").attr("id"));		//tests to display which section has the class "current"
+	    $(".current").removeClass("current");	//removes the current class and all child
 
-	    if(new_current == "#synopsis"){
-	    	$("#synopsis").addClass("current");
+	    if(new_current == "#synopsis"){			//if variable value is equal to ....
+	    	$("#synopsis").addClass("current");		//then add the class current to section...
 	    } else if (new_current == "#characters"){
 	    	$("#characters").addClass("current");
 	    }
@@ -75,21 +75,16 @@ document.addEventListener("DOMContentLoaded",function(){
 
 	//<==============CHARACTERS MODAL======================>
 
-	//$("body").append($overlay);
-	//var $overlay = $("#overlay");
-	//$("#characters").append($overlay);
-	$(".char_list a img").click(function(event){
-		event.preventDefault();
-		var imgAlt = $(this).attr("alt");
+	$(".char_list a img").click(function(event){ //when clicking image in .char_list
+		event.preventDefault();		//stops the default event from running
+		var imgAlt = $(this).attr("alt");	//create variable that stores alt attribute of image we clicked on
 		
-		if (imgAlt=="cobb"){
-			//$overlay.append($(".cobb"));
-			$(".cobb").show();
-			$(".exit").click(function(event){
-				event.preventDefault();
-		 		$(".cobb").hide();
-		 		//$("#overlay .cobb").remove();
-		 	});		
+		if (imgAlt=="cobb"){	//if alt value is equal to ....
+			$(".cobb").show();		//show ...
+			$(".exit").click(function(event){ //if .exit class is clicked
+				event.preventDefault();		//stops the default event from running
+		 		$(".cobb").hide();		//hide the class ...		 	
+		 	});
 		} else if(imgAlt=="mal"){
 			$(".mal").show();
 			$(".exit").click(function(event){
@@ -134,25 +129,88 @@ document.addEventListener("DOMContentLoaded",function(){
 		 	});	
 		}
 
+
+		
+		
+	});
+
+	$(".char_list2 a img").click(function(event){ //when clicking image in .char_list
+		event.preventDefault();		//stops the default event from running
+		var imgAlt = $(this).attr("alt");	//create variable that stores alt attribute of image we clicked on
+		
+		if (imgAlt=="cobb"){	//if alt value is equal to ....
+			$(".cobb").show();		//show ...
+			$(".exit").click(function(event){ //if .exit class is clicked
+				event.preventDefault();		//stops the default event from running
+		 		$(".cobb").hide();		//hide the class ...		 	
+		 	});
+		} else if(imgAlt=="mal"){
+			$(".mal").show();
+			$(".exit").click(function(event){
+				event.preventDefault();
+		 		$(".mal").hide();
+		 	});	
+		} else if(imgAlt=="arthur"){
+			$(".arthur").show();
+			$(".exit").click(function(event){
+				event.preventDefault();
+		 		$(".arthur").hide();
+		 	});	
+		} else if(imgAlt=="ariadne"){
+			$(".ariadne").show();
+			$(".exit").click(function(event){
+				event.preventDefault();
+		 		$(".ariadne").hide();
+		 	});	
+		} else if(imgAlt=="eames"){
+			$(".eames").show();
+			$(".exit").click(function(event){
+				event.preventDefault();
+		 		$(".eames").hide();
+		 	});	
+		} else if(imgAlt=="saito"){
+			$(".saito").show();
+			$(".exit").click(function(event){
+				event.preventDefault();
+		 		$(".saito").hide();
+		 	});	
+		} else if(imgAlt=="fischer"){
+			$(".fischer").show();
+			$(".exit").click(function(event){
+				event.preventDefault();
+		 		$(".fischer").hide();
+		 	});	
+		} else if(imgAlt=="yusuf"){
+			$(".yusuf").show();
+			$(".exit").click(function(event){
+				event.preventDefault();
+		 		$(".yusuf").hide();
+		 	});	
+		}
+		
+
 		
 		
 	});
 	
 	//<=============gallery TABS===============>
-	$(".side_button a img").click(function(event){
-		event.preventDefault();
-		var imgAlt = $(this).attr("alt");
-		if (imgAlt=="wallpapers"){
-			$(".wallpapers").show();
+	$(".side_button a img").click(function(event){ //if image in class is clicked
+		event.preventDefault();		//stops the default event from running
+		var imgAlt = $(this).attr("alt");	//create variable that stores alt attribute of image we clicked on
+		if (imgAlt=="wallpapers"){		//if imgAlt equals the value ...
+			$(".wallpapers").show();	//then show... and hide...
 			$(".posters").hide();
 			$(".screenshots").hide();
 			$(".behind_scenes").hide();
-			$(".soundtracks").hide();
 			$(".wallpaper_text").show();
 			$(".poster_text").hide();
-			$(".soundtrack_text").hide();
 			$(".screenshot_text").hide();
 			$(".behind_text").hide();
+			$(".one").show();
+			$(".two").hide();
+			$(".three").hide();
+			$(".four").hide();			
+
 			
 
 		} else if (imgAlt=="posters"){
@@ -160,102 +218,73 @@ document.addEventListener("DOMContentLoaded",function(){
 			$(".posters").show();
 			$(".screenshots").hide();
 			$(".behind_scenes").hide();
-			$(".soundtracks").hide();
 			$(".wallpaper_text").hide();
 			$(".poster_text").show();
-			$(".soundtrack_text").hide();
 			$(".screenshot_text").hide();
 			$(".behind_text").hide();
+			$(".one").hide();
+			$(".two").show();
+			$(".three").hide();
+			$(".four").hide();	
 
 		} else if (imgAlt=="screenshots"){
 			$(".wallpapers").hide();
 			$(".posters").hide();
 			$(".screenshots").show();
 			$(".behind_scenes").hide();
-			$(".soundtracks").hide();
 			$(".wallpaper_text").hide();
 			$(".poster_text").hide();
-			$(".soundtrack_text").hide();
 			$(".screenshot_text").show();
 			$(".behind_text").hide();
+			$(".one").hide();
+			$(".two").hide();
+			$(".three").show();
+			$(".four").hide();	
 
 		} else if (imgAlt=="behind the scenes"){
 			$(".wallpapers").hide();
 			$(".posters").hide();
 			$(".screenshots").hide();
 			$(".behind_scenes").show();
-			$(".soundtracks").hide();
 			$(".wallpaper_text").hide();
 			$(".poster_text").hide();
-			$(".soundtrack_text").hide();
 			$(".screenshot_text").hide();
 			$(".behind_text").show();
-		
-
-		} else if (imgAlt=="soundtracks"){
-			$(".wallpapers").hide();
-			$(".posters").hide();
-			$(".screenshots").hide();
-			$(".behind_scenes").hide();
-			$(".soundtracks").show();
-			$(".wallpaper_text").hide();
-			$(".poster_text").hide();
-			$(".soundtrack_text").show();
-			$(".screenshot_text").hide();
-			$(".behind_text").hide();		
+			$(".one").hide();
+			$(".two").hide();
+			$(".three").hide();
+			$(".four").show();	
 		}
 	});
 		 	
 	
-	//<=============GALLERY TABS===============>
-	// $(".side_button1 a img").click(function(event){
-	// 	event.preventDefault();
-	// 	var imgAlt = $(this).attr("alt");
-	// 	if (imgAlt=="screenshots"){
-	// 		$(".screenshots").show();
-	// 		$(".behind_scenes").hide();
-	// 		$(".screenshot_text").show();
-	// 		$(".behind_text").hide();
-	// 	} else if (imgAlt=="behind the scenes"){
-	// 		$(".behind_scenes").show();
-	// 		$(".screenshots").hide();
-	// 		$(".behind_text").show();
-	// 		$(".screenshot_text").hide();
-	// 	}
-	// });
-	
-
 	//<============GALLERY IMAGES===============>
 	
-	//add overlay to body
-	
-	// $(".gallery_content .image_list a img").click(function(event){
-	// 	event.preventDefault();
-	// 	$("#gallery").append($overlay);
-	// 	var imgLoc = $(this).attr("src");
-	// 	$image.attr("src", imgLoc);
-	// 	$overlay.show();
-
-	// })
-
-	//<=============gallery IMAGES==============>
-	var $overlay = $('<div id="overlay"></div>');
-	var $image = $("<img>");
+	var $overlay = $('<div id="overlay"></div>'); 	//creates a div  element with id overlay
+	var $image = $("<img>");	//creates image element 
 	//add image to overlay
-	$overlay.append($image);
+	$overlay.append($image);	//adds image to overlay
 
-	$(".gallery_content .image_list a img").click(function(event){
-		event.preventDefault();
-		$("#gallery").append($overlay);
-		var imgLoc = $(this).attr("src");
-		$image.attr("src", imgLoc);
-		$overlay.show();
+	$(".gallery_content .image_list a img").click(function(event){	//selects img from class
+		event.preventDefault();		//stops the default event from running
+		$("#gallery").append($overlay);		//adds overlay to section gallery
+		var imgLoc = $(this).attr("src");	//new variable that stores the src attribute from image that we click on
+		$image.attr("src", imgLoc);		//uses variable imgLoc as source location of variable $image
+		$overlay.show();		//displays the overlay
 
 	})
-	$overlay.click(function(){
-		$overlay.hide();
+	$overlay.click(function(){	//when the overlay div is clicked
+		$overlay.hide();		//hide overlay
 	})
 			
-	
+	// <==============GALLERY EFFECT==============>
+	$(function() {
+	    $(".rslides").responsiveSlides();
+
+	    $(".slider3").responsiveSlides({
+	        manualControls: '.slider3-pager',
+	        maxwidth: 720
+	      });
+		});
 
 });
